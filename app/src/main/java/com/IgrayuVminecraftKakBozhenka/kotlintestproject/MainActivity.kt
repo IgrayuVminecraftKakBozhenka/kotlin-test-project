15c615438db58407dbf88e39a6b3c4395bb9db1b
danil <danko200028@mail.ru>
2021-03-22 16:46:07 +0600

    layout done

--- app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/MainActivity.kt
+++ app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.IgrayuVminecraftKakBozhenka.kotlintestproject
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resources
+        findViewById<TextView>(R.id.tv_question_title).setText(R.string.str)
+        val beginButton = findViewById<Button>(R.id.beginButton)
+        beginButton.setOnClickListener {
+            val intent = Intent(this, Questions::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
+
+
 }
