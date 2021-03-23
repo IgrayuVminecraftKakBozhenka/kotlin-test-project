137b38678cad2a74189e5328f3e7b53efd676fb3
danil <danko200028@mail.ru>
2021-03-23 16:10:21 +0600

    layout fixed

--- app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/MainActivity.kt
+++ app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -13,8 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resources
-        findViewById<TextView>(R.id.tv_question_title).setText(R.string.str)
         val beginButton = findViewById<Button>(R.id.beginButton)
         beginButton.setOnClickListener {
             val intent = Intent(this, Questions::class.java)
