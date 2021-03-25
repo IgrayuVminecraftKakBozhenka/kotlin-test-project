3e4442bdaa5de0f50a7949381eb1dc7be684fcd4
danil <danko200028@mail.ru>
2021-03-25 10:19:15 +0600

    question class refactored

--- app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/Question.kt
+++ app/src/main/java/com/IgrayuVminecraftKakBozhenka/kotlintestproject/Question.kt
@@ -3,13 +3,8 @@ package com.IgrayuVminecraftKakBozhenka.kotlintestproject
 class Question(
         val id: Int,
         val question: String,
-        val answer: String,
+        val correctAnswerID: Int,
         val reward: Int,
-        val incorrectAnswerOne: String,
-        val incorrectAnswerTwo: String,
-        val incorrectAnswerThree: String
-) {
-    fun checkAnswer(_answer: String): Boolean {
-        return answer == _answer
-    }
-}
+        val Answers: Array<Int>
+
+)
